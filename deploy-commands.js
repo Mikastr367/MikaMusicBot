6df742d873cc96939a1e55c8710f9b53992fecb3
Mikastr367 <mikastratmann22@gmail.com>
2025-11-11 13:50:51 +0100

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v10');
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+for (const file of fs.readdirSync(commandsPath).filter(f => f.endsWith('.js'))) {
+  const cmd = require(path.join(commandsPath, file));
+  if (cmd.data) commands.push(cmd.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Deploying commands...');
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands }
+    );
+    console.log('✅ Commands registered to guild', process.env.GUILD_ID);
+  } catch (err) {
+    console.error(err);
+  }
+})();
